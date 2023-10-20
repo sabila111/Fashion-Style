80194b5cdee88734f1b603159ef24c15e6b0c71e
sabila111 <sharminsharker2002@gmail.com>
2023-10-21 01:44:31 +0600

    fixed route name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -14,50 +14,77 @@ console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const productCollection = client.db('productDB').collection('product')
+        const productCollection = client.db('productDB').collection('product')
 
-    app.get('/product/:brandName', async(req, res)=>{
+        app.get('/product', async(req, res)=>{
 
-        const brandName = req.params.brandName
-        const cursor = productCollection.find({brand: brandName})
-        const result = await cursor.toArray()
+            const cursor = productCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+      app.get('/product/:name/:id', async(req, res)=>{
+        const name = req.params.name
+        const id = req.params.id
+        const query = {_id: new ObjectId(id ),
+            brand:name
+        }
+        const result= await productCollection.findOne(query)
         res.send(result)
-    })
+      })
+    //   app.get('/product/:name/update/:id', async(req, res)=>{
+    //     const name = req.params.name
+    //     const id = req.params.id
+    //     const query = {_id: new ObjectId(id ),
+    //         brand:name
+    //     }
+    //     const result= await productCollection.findOne(query)
+    //     res.send(result)
+    //   })
 
-    app.post('/product', async(req, res)=>{
-        const addProduct = req.body
-        console.log(addProduct)
-        const result = await productCollection.insertOne(addProduct)
-        res.send(result)
-    })
+        app.get('/product/:name', async(req, res)=>{
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+            const name = req.params.name
+            const cursor = productCollection.find({brand:name})
+            const result = await cursor.toArray()
+            res.send(result)
+            console.log('add korchi', name)
+        })
+
+        app.post('/product', async (req, res) => {
+            const addProduct = req.body
+            console.log(addProduct)
+            const result = await productCollection.insertOne(addProduct)
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
     res.send('style and fashion server is Running')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`style and fashion server is Running on port ${port}`)
-  })
+})
